aff6ae61c2a7715edb93fb35b2df0f65aacf4541
tenisistaa <krzychu_ten@o2.pl>
2018-08-29 14:36:17 +0200

    photos: studi, kids

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -19,35 +19,61 @@ const showImage = (photo) => {
 }
 
 const AboutUs = () => (
-  <div className="aboutUs">
-    <h1> Studio </h1>
-    <p> Działamy od 1997r uwieczniając na fotografiach najważniejsze chwile z życia naszych Klientów.
-      Od 21 lat zajmujemy się fotografią rodzinną, biznesową, sesjami przedszkolnymi, ślubnymi, komunijnymi.
-          Wieloletnie doświadczenie i indywidualne podejście do każdego klienta pozwala nam spełniać oczekiwania Klientów i czerpać radość z wykonywanej pracy.</p>
-    <p> To co robimy, to dla nas nie tylko praca - fotografia jest naszą pasją.
-      Do każdego klienta podchodzimy indywidualnie, na każdą sesję jesteśmy przygotowani.
-      Spełnimy Państwa oczekiwania, zrealizujemy marzenia, podsuniemy propozycje, pokażemy przykładowe aranżacje zdjęć.
-          Zapraszamy do odwiedzania naszego profilu na którym pojawiać się będą aktualne oferty oraz promocje.</p>
-    <p> Współpracujemy z doskonałymi wizażystkami, podczas sesji zadbamy o makijaż naszych modelek,
-          a specjalny kącik fryzjerski z myjką do włosów pozwoli na profesjonalne ułożenie włosów.</p>
-    <div className="photo-gallery">
-      <div className="photos photo1" onClick={() => showImage('photo1')}>
-        {/* <img src="images/studio5.jpg" alt="studio5" width="200px" height="100px"/> */}
-      </div>
-      <div className="photos photo2" onClick={() => showImage('photo2')}></div>
-      <div className="photos photo3" onClick={() => showImage('photo3')}></div>
-      <div className="photos photo4" onClick={() => showImage('photo4')}></div>
-    </div>
-    <p> Zapraszamy do naszego jasnego, klimatycznego studia. Częstujemy kawą, herbatą, znajdzie się też coś słodkiego ;-) </p>
-    <p> Zapraszamy do zapoznania się z naszą ofertą.</p>
+  <div className="about-us">
+    <div className="container">
 
-    <h1> Przedszkole </h1>
-    <p> Dużo radości i satysfakcji sprawiają nam sesje zdjęciowe wykonywane w przedszkolach.</p>
-    <p> Praca z dziećmi to najwspanialsza przygoda, która może spotkać dorosłego człowieka.
-    Każdy mały przedszkolak jest inny, do każdego trzeba podejść indywidualnie.
+      <h1> Studio </h1>
+      <p> Działamy od 1997r uwieczniając na fotografiach najważniejsze chwile z życia naszych Klientów.
+        Od 21 lat zajmujemy się fotografią rodzinną, biznesową, sesjami przedszkolnymi, ślubnymi, komunijnymi.
+          Wieloletnie doświadczenie i indywidualne podejście do każdego klienta pozwala nam spełniać oczekiwania Klientów i czerpać radość z wykonywanej pracy.</p>
+      <p> To co robimy, to dla nas nie tylko praca - fotografia jest naszą pasją.
+        Do każdego klienta podchodzimy indywidualnie, na każdą sesję jesteśmy przygotowani.
+        Spełnimy Państwa oczekiwania, zrealizujemy marzenia, podsuniemy propozycje, pokażemy przykładowe aranżacje zdjęć.
+          Zapraszamy do odwiedzania naszego profilu na którym pojawiać się będą aktualne oferty oraz promocje.</p>
+      <p> Współpracujemy z doskonałymi wizażystkami, podczas sesji zadbamy o makijaż naszych modelek,
+          a specjalny kącik fryzjerski z myjką do włosów pozwoli na profesjonalne ułożenie włosów.</p>
+    </div>
+    <div className="photo-gallery">
+      <div className="photo-box">
+        <div className="photos photo1" onClick={() => showImage('photo1')}>
+          {/* <img src="images/studio5.jpg" alt="studio5" width="200px" height="100px"/> */}
+        </div>
+      </div>
+      <div className="photo-box">
+        <div className="photos photo2" onClick={() => showImage('photo2')}></div>
+      </div>
+      <div className="photo-box">
+        <div className="photos photo3" onClick={() => showImage('photo3')}></div>
+      </div>
+    </div>
+    <div className="container">
+
+      <p> Zapraszamy do naszego jasnego, klimatycznego studia. Częstujemy kawą, herbatą, znajdzie się też coś słodkiego ;-) </p>
+      <p> Zapraszamy do zapoznania się z naszą ofertą.</p>
+
+      <h1> Przedszkole </h1>
+      <p> Dużo radości i satysfakcji sprawiają nam sesje zdjęciowe wykonywane w przedszkolach.</p>
+      <p> Praca z dziećmi to najwspanialsza przygoda, która może spotkać dorosłego człowieka.
+      Każdy mały przedszkolak jest inny, do każdego trzeba podejść indywidualnie.
         Największa radość to uśmiech dziecka po drugiej stronie aparatu: rozluźniony, szczęśliwy, naturalny.</p>
-    <p> Sesje zdjęciowe w przedszkolach wykonujemy już od 21lat.
+    </div>
+    <div className="photo-gallery">
+      <div className="photo-box">
+        <div className="photos kids1" onClick={() => showImage('kids1')}>
+          {/* <img src="images/studio5.jpg" alt="studio5" width="200px" height="100px"/> */}
+        </div>
+      </div>
+      <div className="photo-box">
+        <div className="photos kids2" onClick={() => showImage('kids2')}></div>
+      </div>
+      <div className="photo-box">
+        <div className="photos kids3" onClick={() => showImage('kids3')}></div>
+      </div>
+    </div>
+    <div className="container">
+      <p> Sesje zdjęciowe w przedszkolach wykonujemy już od 21lat.
       Działamy na terenie całego śląska. </p>
+    </div>
   </div>
 );
 
--- src/components/Studio.js
+++ src/components/Studio.js
@@ -7,72 +7,77 @@ import Kindergarten from './Kindergarten'
 import Contact from './Contact'
 import AboutUs from './AboutUs'
 
-// var last_known_scroll_position = 0;
-// var ticking = false;
+function doSomething(scroll_pos) {
+  var banner = document.querySelector('.banner');
+  var bannerContainer = document.querySelector('.banner-container');
+  var backColor = document.querySelector('.back-color');
+  var pos = scroll_pos;
+  var navBelt = document.querySelector('.belt-nav');
+  var aboutUs = document.querySelector('.about-us');
+  var navBar = document.querySelector('nav');
+  var navBarPosition = navBar.getBoundingClientRect().top;
 
-// function doSomething(scroll_pos) {
-//   var banner = document.querySelector('.banner');
-//   var pos = scroll_pos;
-//   // do something with the scroll position
-//   console.log("działa", scroll_pos);
-//   console.log(`position: ${scroll_pos} px`);
-//   banner.style = `transform: translateY( ${-pos/2}px)`;
-// }
+  if (window.innerHeight < window.scrollY + 70) {
+    if (!navBelt.classList.contains('belt-nav-top')) {
+      navBelt.classList.add('belt-nav-top');
+      backColor.style = 'transform: translateY(0)';
+    }
+  } else {
+    if (navBelt.classList.contains('belt-nav-top')) {
+      navBelt.classList.remove('belt-nav-top');
+      backColor.style.cssText = '';
+    }
+  }
+  banner.style = `transform: translateY( ${-pos / 2}px)`;
+}
 
-// window.addEventListener('scroll', function(e) {
-
-//   last_known_scroll_position = window.scrollY;
-
-//   if (!ticking) {
-
-//     window.requestAnimationFrame(function() {
-//       doSomething(last_known_scroll_position);
-//       ticking = false;
-//     });
-     
-//     ticking = true;
-
-//   }
-  
-// });
+window.addEventListener('scroll', function (e) {
+  window.requestAnimationFrame(function () {
+    doSomething(window.scrollY);
+  });
+});
 
 const Studio = () => (
   <div>
-    <div className="bannerAndNav">
+    <div className="banner-container">
       <div className="banner"></div>
     </div>
-    <div className="backColor">
+    <div className="belt-nav">
       <div className="container">
-      <nav>
-        <ul>
-          <li>
-            <Link to="/studio"> Studio </Link>
-          </li>
-          <li>
-            <Link to="/studio/offer"> Oferta </Link>
-          </li>
-          <li>
-            <Link to="/studio/gallery"> Galeria </Link>
-          </li>
-          <li>
-            <Link to="/kindergarten"> Przedszkole </Link>
-          </li>
-          <li>
-            <Link to="studio/contact"> Kontakt </Link>
-          </li>
-        </ul>
-      </nav>
-      {/* <div id="logo"></div> */}
-      <Switch>
-        <Route exact path="/studio" component={AboutUs} />
-        <Route exact path="/studio/gallery" component={StudioGallery} />
-        <Route exact path="/studio/offer" component={StudioOffer} />
-        <Route exact path="/kindergarten" component={Kindergarten} />
-        <Route exact path="/studio/contact" component={Contact} />
-      </Switch>
+        <nav>
+          <Link to="/"><div id="logo"></div></Link>
+          <ul>
+            <li>
+              <Link to="/studio"> Studio </Link>
+            </li>
+            <li>
+              <Link to="/studio/offer"> Oferta </Link>
+            </li>
+            <li>
+              <Link to="/studio/gallery"> Galeria </Link>
+            </li>
+            <li>
+              <Link to="/kindergarten"> Przedszkole </Link>
+            </li>
+            <li>
+              <Link to="studio/contact"> Kontakt </Link>
+            </li>
+          </ul>
+        </nav>
       </div>
     </div>
-  </div>
+    <div className="back-color">
+      {/* <div className="container"> */}
+        <Switch>
+          <Route exact path="/studio" component={AboutUs} />
+          <Route exact path="/studio/gallery" component={StudioGallery} />
+          <Route exact path="/studio/offer" component={StudioOffer} />
+          <Route exact path="/kindergarten" component={Kindergarten} />
+          <Route exact path="/studio/contact" component={Contact} />
+        </Switch>
+      {/* </div> */}
+    </div>
+  </div >
 
 
 );
