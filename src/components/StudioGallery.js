355785ebdb12fc674c2d02dcaea71c8a399bc4e9
tenisistaa <krzychu_ten@o2.pl>
2018-09-10 23:33:41 +0200

    our studio

--- src/components/StudioGallery.js
+++ src/components/StudioGallery.js
@@ -6,63 +6,74 @@ export default class StudioGallery extends React.Component {
 
   componentDidMount() {
     menuUp("about-us-studio-gallery");
+    this.showImg();
+  }
+
+  showImg = () => {
+    const bigPhoto = document.getElementById('big-photo');
+    const miniPhotos = document.querySelectorAll('.mini-img');
+    for(let i = 0; i < miniPhotos.length; i++) {
+      miniPhotos[i].addEventListener('click', function(event) {
+        if(event.target.tagName === 'IMG') {
+          bigPhoto.src = event.target.src;
+        }
+      })
+    }
   }
 
   render() {
     return (
       <div id='about-us-studio-gallery'>
         <div className='container'>
-          <h1 className='title'> Zapraszamy do galerii naszych zdjęć </h1>
+          <h1 className='title'> Nasze studio </h1>
         </div>
-        <div className='photo-gallery photo-kids'>
           <div className='photo-box-gallery'>
             <div className='photo-box-mini'>
-              <img
-                src='/gallery/studio01.jpg' alt='studio01'
-              ></img>
-              <img
-                src='/gallery/studio02.jpg' alt='studio02'
-              ></img>
-              <img
-                src='/gallery/studio03.jpg' alt='studio03'
-              ></img>
-        
+              <div className="mini-img">
+                <img
+                  src='/gallery/studio01.jpg' alt='studio01'
+                ></img>
+              </div>
+              <div className="mini-img">
+                <img
+                  src='/gallery/studio02.jpg' alt='studio02'
+                ></img>
+              </div>
+              <div className="mini-img">
+                <img
+                  src='/gallery/studio03.jpg' alt='studio03'
+                ></img>
+              </div>
+
             </div>
             <div className='photo-box-big'>
-              <img
+              <img id='big-photo'
                 src='/gallery/studio01.jpg' alt='studio01'
               ></img>
             </div>
-            <div className='photo-box-mini'>
-              <img
-                src='/gallery/studio04.jpg' alt='studio04'
+            <div className="photo-box-mini">
+              <div className='mini-img'>
+                <img
+                  src='/gallery/studio04.jpg' alt='studio04'
                 >
-              </img>
-              <img
-                src='/gallery/studio05.jpg' alt='studio04'
+                </img>
+              </div>
+              <div className='mini-img'>
+                <img
+                  src='/gallery/studio05.jpg' alt='studio05'
                 >
-              </img>
-              <img
-                src='/gallery/studio06.jpg' alt='studio04'
-              >
-              </img>
+                </img>
+              </div>
+              <div className='mini-img'>
+                <img
+                  src='/gallery/studio06.jpg' alt='studio06'
+                >
+                </img>
+              </div>
+
             </div>
           </div>
-          {/* <div className='photo-box'>
-        <div className='photos kids3'></div>
-      </div>
-      <div className='photo-box'>
-        <div className='photos kids5'></div>
-      </div> */}
-        </div>
-        <div className='container'>
-          <p> Sesje zdjęciowe wykonywane w przedszkolach sprawiają nam dużo radości i satysfakcji.</p>
-          <p> Praca z dziećmi to najwspanialsza przygoda, która może spotkać dorosłego człowieka.
-            Każdy mały przedszkolak jest inny, do każdego trzeba podejść indywidualnie.
-        Największa radość to uśmiech dziecka po drugiej stronie aparatu: rozluźniony, szczęśliwy, naturalny.</p>
-          <p> Sesje zdjęciowe w przedszkolach wykonujemy już od 21 lat.<br />
-            Działamy na terenie całego śląska. </p>
-        </div>
+        
         <div className='panorama'></div>
         <footer>Wszelkie prawa zastrzeżone</footer>
       </div>
