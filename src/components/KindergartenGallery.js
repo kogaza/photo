1ba1f2c3bad92326eb4cd294ae8f11c6fb628ba9
Krzysztof Ogaza <krzychu_ten@o2.pl>
2018-11-07 10:37:19 +0100

    create json gallery files

--- src/components/KindergartenGallery.js
+++ src/components/KindergartenGallery.js
@@ -4,43 +4,42 @@ import { menuUp } from './functions'
 import GalleryVertical from './GalleryVertical';
 import GalleryHorizontal from './GalleryHorizontal';
 import GalleryMixed from './GalleryMixed';
+import gallery from './json/kindergartenPhotos.json'
 
 export default class KindergartenGallery extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      titleAutumn: 'Sesje jesienne',
-      photosAutumn: [
-        'jesien/01.jpg', 'jesien/02.jpg', 'jesien/03.jpg',
-        'jesien/04.jpg', 'jesien/03.jpg', 'jesien/06.jpg',
-        'jesien/07.jpg', 'jesien/08.jpg', 'jesien/09.jpg',
-        'jesien/10.jpg', 'jesien/11.jpg', 'jesien/12.jpg',
-        'jesien/13.jpg', 'jesien/14.jpg', 'jesien/15.jpg',
-        'jesien/16.jpg', 'jesien/17.jpg', 'jesien/18.jpg',
-        'jesien/19.jpg', 'jesien/20.jpg', 'jesien/21.jpg'
-      ],
-      titleSpring: 'Sesje wiosenne',
-      photosSpring: [
-        'wiosna/01.jpg', 'wiosna/02.jpg', 'wiosna/03.jpg',
-        'wiosna/09.jpg', 'wiosna/05.jpg', 'wiosna/06.jpg', 
-        'wiosna/08.jpg', 'wiosna/11.jpg',
-        'wiosna/12.jpg', 'wiosna/13.jpg', 'wiosna/15.jpg',
-        'wiosna/16.jpg', 'wiosna/17.jpg', 'wiosna/14.jpg'
-      ],
+      titleAutumn: '',
+      photosAutumn: [],
+      titleSpring: '',
+      photosSpring: [],
+      titleWinter: '',
+      photosWinter: [],
     }
   }
-
+  componentWillMount() {
+    this.setState({
+      titleAutumn: gallery.autumn.title,
+      photosAutumn: gallery.autumn.photos,
+      titleSpring: gallery.spring.title,
+      photosSpring: gallery.spring.photos,
+      titleWinter: gallery.winter.title,
+      photosWinter: gallery.winter.photos
+    })
+  }
   componentDidMount() {
     menuUp("about-us-kindergarten-gallery");
   }
-
   render() {
     const { fullPhoto } = this.props;
     const {
       titleAutumn,
       photosAutumn,
       titleSpring,
-      photosSpring
+      photosSpring,
+      titleWinter,
+      photosWinter
     } = this.state;
     return (
       <div id='about-us-kindergarten-gallery'>
@@ -54,6 +53,13 @@ export default class KindergartenGallery extends React.Component {
         <GalleryMixed
           photos={photosSpring}
           title={titleSpring}
+          fullPhoto={fullPhoto}
+          showPhoto={(photo, all) => this.props.showPhoto(photo, all)}
+        />
+        <div className='line'></div>
+        <GalleryVertical
+          photos={photosWinter}
+          title={titleWinter}
           fullPhoto={fullPhoto}
           showPhoto={(photo, all) => this.props.showPhoto(photo, all)}
         />
--- src/components/StudioGallery.js
+++ src/components/StudioGallery.js
@@ -3,38 +3,34 @@ import '../App.css';
 import { menuUp } from './functions';
 import GalleryVertical from './GalleryVertical';
 import GalleryHorizontal from './GalleryHorizontal';
+import gallery from './json/studioPhotos.json'
+
 
 export default class StudioGallery extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      titleStudio: 'Nasze studio',
-      photosStudio: [
-        'studio01.jpg', 'studio02.jpg', 'studio03.jpg',
-        'studio04.jpg', 'studio05.jpg', 'studio06.jpg',
-        'studio07.jpg', 'studio08.jpg', 'studio09.jpg',
-        'studio10.jpg', 'studio11.jpg', 'studio12.jpg'
-      ],
-      titleOther: 'Nasze zdjęcia',
-      photosOther: [
-        'other01.jpg', 'other02.jpg', 'other03.jpg',
-        'other04.jpg', 'other05.jpg', 'other06.jpg',
-        'other07.jpg', 'other08.jpg', 'other09.jpg'
-      ],
-      titleSesion: 'Nasze sesje',
-      photosSesion: [
-        'session01.jpg', 'session02.jpg', 'session03.jpg',
-        'session04.jpg', 'session05.jpg', 'session06.jpg',
-        'session07.jpg'
-      ],
-
+      titleStudio: '',
+      photosStudio: [],
+      titleOther: '',
+      photosOther: [],
+      titleSesion: '',
+      photosSesion: [],
     }
   }
-
+  componentWillMount() {
+    this.setState({
+      titleStudio: gallery.studio.title,
+      photosStudio: gallery.studio.photos,
+      titleOther: gallery.other.title,
+      photosOther: gallery.other.photos,
+      titleSesion: gallery.sesion.title,
+      photosSesion: gallery.sesion.photos
+    })
+  }
   componentDidMount() {
     menuUp("about-us-studio-gallery");
   }
-
   render() {
     const { fullPhoto } = this.props;
     const {
